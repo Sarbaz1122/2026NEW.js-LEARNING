09a7d2f107ced87faa77309b2391b361d0bee350
Sarbaz1122 <sarbazafridi0@gmail.com>
2026-07-01 10:27:31 +0500

    added javascript assignments

---
+++ Variable/assignment.js
@@ -0,0 +1,31 @@
+// ASSIGNMENT NO.1
+let name = "muhammad allah";
+age = 23;
+city="khyber";
+const language = "javascript";
+const country = "pakistan";
+console.log(name,age ,city, language,country);
+
+//ASSIGNMENT NO.2
+// TO UPDATE THE VALUE OF VARIABLE 
+
+// var product = "Laptop";
+// var product  = "mobile" 
+
+// let price = 50000;
+// price = "30000"
+
+// const company = "HP";
+// const company= "dell";
+
+// // ASSIGNMENT NO.3
+// userName  // correct
+// User_Name    // correct
+// $user        // correct
+// 123name     //wrong
+// full-name    // correct
+// myAge       // correct
+// const        //wrong
+// student1     // correct
+// _firstName  // correct
+// last Name      //wrong
---
+++ Variable/smallProject.js
@@ -0,0 +1,42 @@
+            //////STUDENT INFORMATION DETAILS//////
+            let studentName = "ali";
+            let studentFatherName ="ahmed";
+            let studentAge = 23;
+            let studentClass = "11th";
+            let studentRollNo =93939;
+            const schoolName = "city school";
+
+            console.log("Name:" + studentName);
+            console.log("Father's Name:" + studentFatherName);
+            console.log("Age:" + studentAge);
+            console.log("Class:" + studentClass);
+            console.log("Roll No:" + studentRollNo);
+            console.log("School Name:" + schoolName);
+
+
+            /////Challenge Questions//
+            //Q1.Why can var be re-declared but let cannot?
+            //ans // ans. Because var allows re-declaration in the same scope,
+// while let does not. Also, var is function-scoped and let is block-scoped.
+
+               //Q2. why const must have an initial value?
+
+               //ans.const must have an initial value because its 
+               // value is fixed and cannot be reassigned after declaration.
+
+
+
+            //Q3. Which keyword should you use most in modern JavaScript: var, let, or const?
+            //ans. let and const are use in modern js 
+
+            //Q4. What happens if you write:
+//             const age = 20;
+// age = 25;
+  // ans. It creates an error in the console because const variables
+// cannot be reassigned after declaration.
+
+  //Q5. Is myName the same as myname? Why?
+    //ans.// ans. No, they are not the same because JavaScript is a case-sensitive language. 
+            
+
+
