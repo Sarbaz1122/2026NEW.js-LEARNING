29949bb2c8aaaa9634964aa4e4c6ad04699cb134
Sarbaz1122 <sarbazafridi0@gmail.com>
2026-07-02 10:01:26 +0500

    add assignments in dataTypes file

---
+++ DATATYPES/PrimitiveDatatypes/Assign.js
@@ -0,0 +1,49 @@
+/// Assign No.1
+// String Assignment
+let name = "khan";
+let city = "Karachi";
+const favoriteLanguage= "javascript";
+console.log(name, city, favoriteLanguage);
+
+/// Assign No.2
+// Boolean Assignment
+let isStudent = true;
+let isMarried = true;
+let hasLaptop = false;
+console.log(isStudent);
+console.log(isMarried);
+console.log(hasLaptop);
+
+
+// Assign No.3
+//Undefined Assignment
+let email;
+console.log(email);
+
+///Question: WHAT IS printed in the console ? WHY ?
+// Answer: 
+
+
+// Assign No.4
+// Null Assignment
+let middleName = null;
+console.log(middleName)
+
+// Q: why do we use Null?
+
+// Assign No.5
+// BigInt Assignment
+let population = BigInt("123456789012345678901234567890123");
+console.log(population)
+
+///Q. why can,t a normal number store this safely?
+
+// Assign No.6
+// Symbol Assignment
+let id1 = "user";
+let id2 = "user";
+console.log(id1 === id2);
+
+/// Q.. why is the result false?
+
+
