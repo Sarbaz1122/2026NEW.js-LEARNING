dcc21717f894bef25cf84771f295d305849d7176
Sarbaz1122 <sarbazafridi0@gmail.com>
2026-07-03 11:56:07 +0500

    add some more operators

---
+++ Operators/Assignment-OP.js
@@ -0,0 +1,49 @@
+// ASSIGNMENT OPERATORS
+// e.g +=
+
+console.log("ASSIGNMENT OPERATORS");
+
+
+let a = 5;
+let b = 2;
+a += 4;
+console.log("a += 4:", a); // a = a + 4 {mean that}
+
+// e.g -=
+let c = 6;
+let d = 4;
+c -=2;
+console.log("c-=:",c); // c = c - 2
+
+// e.g *=
+
+let e = 7;
+let f = 2;
+e *= 3;
+console.log("e*:",e); // e = e * 3
+
+// e.g /=
+
+let g = 8;
+let i = 5;
+g /= 2;
+console.log("g/:",g); // g = g / 3
+
+
+// e.g %=
+
+
+
+let u = 8;
+let v = 5;
+g %= 2;
+console.log("g%:",g); // g = g % 3
+
+// e.g **=
+
+let x = 2;
+let y = 4;
+x **= 3;
+console.log("x**:",x);
+
+
---
+++ Operators/Comperision-OP.js
@@ -0,0 +1,22 @@
+console.log("COMPARISON OPERATOR");
+
+// NO.1 == equal to 
+let aa = 8; /// aa for just declaration
+let bb =6;
+
+console.log("6==8",aa==bb);  // US FALSE
+
+/// IF WE WRITE 
+
+let ab = 6; /// aa for just declaration
+let ba =6;
+
+console.log("6==6",ab==ba);  // US TRUE
+
+// NO.2 == Not equal to 
+let ca = 4;
+let ac = 3;
+console.log("4 != 3" , ca!= ac); // give us true because 4 is not equal to 3
+// console.log("3 != 3" , ca!= ac); // if we write like this it give us false because 3 is  equal to 3
+
+
